b5cda3c691322200d51a6b8ca3a7dc8120fea91d
Alaa Eddine <alaneddin91@gmail.com>
2021-01-23 21:58:09 +0100

    adds loading & error handling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 // Components
 import Footer from "../footer/Footer";
 import Banner from "../banner/Banner";
 import Navbar from "../navbar/Navbar";
 import Product from "../products/Product";
+// Bootstrap
+import { Spinner } from "react-bootstrap";
 // CSS
 import "./Home.css";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getProducts();
   }, []);
 
   const getProducts = async () => {
+    setLoading(true);
+    setError(false);
     try {
       const response = await fetch("http://localhost:4004/products", {
         method: "GET",
@@ -22,32 +29,56 @@ const Home = () => {
       });
       const data = await response.json();
       setProducts(data);
+      setLoading(false);
     } catch (error) {
+      setError(true);
       console.log(error);
     }
   };
 
   return (
     <>
-      <Navbar />
-      <Banner />
-      <div style={{ marginTop: -250 }} className="container-lg">
-        <div className="row d-flex justify-content-center align-items-center">
-          {products.map((product, idx) => (
-            <Product
-              key={idx}
-              id={product.id}
-              title={product.name}
-              description={product.description}
-              price={product.price}
-              img={product.imageUrl}
-              rate={products[0].reviews[0].rate}
-            />
-          ))}
-        </div>
-      </div>
+      {error ? (
+        <>
+          <h1> An error occured, please refresh and try again </h1>
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <Banner />
+          {loading ? (
+            <>
+              <Spinner
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                animation="border"
+              ></Spinner>
+              <h1>Loading...</h1>
+            </>
+          ) : (
+            <div style={{ marginTop: -250 }} className="container-lg">
+              <div className="row d-flex justify-content-center align-items-center">
+                {products.map((product, idx) => (
+                  <Product
+                    key={idx}
+                    id={product.id}
+                    title={product.name}
+                    description={product.description}
+                    price={product.price}
+                    img={product.imageUrl}
+                    rate={products[0].reviews[0].rate}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
 
-      <Footer />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
